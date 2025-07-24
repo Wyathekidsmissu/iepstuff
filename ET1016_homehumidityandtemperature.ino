d6ad7bfb4ec12ad65194126dea598a864b7ac74d
Wyathekidsmissu <joshuaj.25@ichat.sp.edu.sg>
2025-07-24 15:40:22 +0800

    Add files via upload

---
+++ ET1016_homehumidityandtemperature.ino
@@ -0,0 +1,17 @@
+
+//title: AIR CONDITIONER AND DEHUMIDIFIER
+//name :joshua jayan and ashton chew
+//admission number 2533580, 2516662
+//class DCEP/FT/1A/09
+//discription:
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
