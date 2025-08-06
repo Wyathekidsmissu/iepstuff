1c84b04f4e1ee4dc75084346acd9490265dfaf91
Wyathekidsmissu <joshuaj.25@ichat.sp.edu.sg>
2025-08-06 09:22:05 +0800

    Update ET1016_homehumidityandtemperature.ino

--- ET1016_homehumidityandtemperature.ino
+++ ET1016_homehumidityandtemperature.ino
@@ -4,14 +4,31 @@
 //admission number 2533580, 2516662
 //class DCEP/FT/1A/09
 //discription:
+#include <Wire.h>
+#include "RichShieldDHT.h"
+#include "RichShieldTM1637.h"
+#define CLK 10
+#define DIO 11
+TM1637 disp(CLK,DIO);
 
+DHT dht;
 
-void setup() {
-  // put your setup code here, to run once:
-
+void setup() 
+{
+	disp.init();  
+	dht.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+float humidity = dht.readHumidity();
+if (isnan(currentHumidity))
+{
+displayError();
 }
+else
+{
+display
+
+
+
+
+
