68abf17ad38a795d3375754e58b2079dc248caa0
Wyathekidsmissu <joshuaj.25@ichat.sp.edu.sg>
2025-08-10 22:23:58 +0800

    Update ET1016_homehumidityandtemperature.ino

--- ET1016_homehumidityandtemperature.ino
+++ ET1016_homehumidityandtemperature.ino
@@ -8,7 +8,10 @@
 
 #include "RichShieldDHT.h"
 #include "RichShieldTM1637.h"
+#include "RichShieldPassiveBuzzer.h"
 
+
+//components to be used
 #define CLK 10
 #define DIO 11
 #define PassiveBuzzerPin 3
@@ -18,6 +21,29 @@
 #define KEY2_PIN 8
 #define BUTTONK1 8
 #define BUTTONK2 9
+
+//declaring of musical notes
+#define NOTE_L1  262
+#define NOTE_L2  294
+#define NOTE_L3  330
+#define NOTE_L4  349
+#define NOTE_L5  392
+#define NOTE_L6  440
+#define NOTE_L7  494
+#define NOTE_M1  523
+#define NOTE_M2  587
+#define NOTE_M3  659
+#define NOTE_M4  698
+#define NOTE_M5  784
+#define NOTE_M6  880
+#define NOTE_M7  988
+#define NOTE_H1  1046
+#define NOTE_H2  1175
+#define NOTE_H3  1318
+#define NOTE_H4  1397
+#define NOTE_H5  1568
+#define NOTE_H6  1760
+#define NOTE_H7  1976
 
 
 
@@ -95,7 +121,21 @@ void displayError()
 {
   disp.display(3,14);//display "E"
 }
-	
+
+void sing() {
+  // iterate over the notes of the melody:
+    int size = sizeof(doubleTiger) / sizeof(int);
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+      int noteDuration = 500 / tempo[thisNote];//a beat for 0.5 second
+
+      buz.playTone(doubleTiger[thisNote], noteDuration);
+
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 0.30;
+      delay(pauseBetweenNotes);
+    }
+}
 	
 
 
